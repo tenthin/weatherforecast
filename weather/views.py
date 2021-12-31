81fccdf9f7c5606eb0d8bd2b4b5a72e449857459
Tenzin <tenthinten@gmail.com>
2021-12-31 23:20:15 +0530

    adding weather api

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    city = 'Las Vegas'
+    
     return render(request,'weather/weather.html')
