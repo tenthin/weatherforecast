84dc5319882c2c3a3a5e057158816901f70d1c3f
Tenzin <tenthinten@gmail.com>
2022-01-01 19:02:57 +0530

    create models and migrate

--- weather/views.py
+++ weather/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-    city = 'Las Vegas'
+    city = 'Bengaluru'
     
     r = requests.get(url.format(city)).json()
     
