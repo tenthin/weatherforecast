3e0e321eb63343f77d68f203bb1affb8ded398a7
Tenzin <tenthinten@gmail.com>
2022-01-01 19:53:22 +0530

    create form to enter name of city

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,16 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     city = 'Bengaluru'
+
+    if request.method == 'POST':
+        pass
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -26,5 +32,5 @@ def index(request):
 
     print(weather_data)
 
-    context = {'weather_data' : weather_data}
+    context = {'weather_data' : weather_data,'form':form}
     return render(request,'weather/weather.html',context)
