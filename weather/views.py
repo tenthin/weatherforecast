e4adf9220cf0c1b83f47bc9b6c3c22a8168ea2cd
Tenzin <tenthinten@gmail.com>
2022-01-01 18:50:34 +0530

    weather city, temperature, description added and executed

--- weather/views.py
+++ weather/views.py
@@ -2,10 +2,18 @@ import requests
 from django.shortcuts import render
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     city = 'Las Vegas'
     
-    r = requests.get(url.format(city))
-    print(r.text)
+    r = requests.get(url.format(city)).json()
+    
 
-    return render(request,'weather/weather.html')
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+    context = {'city_weather' : city_weather}
+    return render(request,'weather/weather.html',context)
